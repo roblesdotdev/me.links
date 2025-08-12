000b6702a885177cd80f18416063a1cf077922c9
roblesdotdev <httpsnip@gmail.com>
2025-08-11 21:30:41 -0300

    setup tailwind css

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
