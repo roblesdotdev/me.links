76efb4f22fd1fdb611f5419b22dc227505053c4d
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2025-08-11 21:26:17 -0300

    Core/dev exp (#1)

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   server: {
-    port: 3000
-  }
-});
+    port: 3000,
+  },
+})
